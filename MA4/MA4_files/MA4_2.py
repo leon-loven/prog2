ede4c04bd0ea925ee18955062220dc99129e96eb
leon-loven <82042705+leon-loven@users.noreply.github.com>
2021-10-11 11:36:43 +0200

    Add files via upload

---
+++ MA4/MA4_files/MA4_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from integer import Integer
+
+def main():
+	f = Integer(5)
+	print(f.get())
+	f.set(7)
+	print(f.get())
+	print(f.fib())
+
+if __name__ == '__main__':
+	main()
