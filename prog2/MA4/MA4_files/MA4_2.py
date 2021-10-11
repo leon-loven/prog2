bab5c2bb0606a6482b1ec71711fbe760e2cee5a0
Leon Lovén <leon.loven99@gmail.com>
2021-10-11 14:27:19 +0200

    Timed Fibonacci codes

--- prog2/MA4/MA4_files/MA4_2.py
+++ prog2/MA4/MA4_files/MA4_2.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
 
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+	timep = []
+	timec = []
+	nlist = [n for n in range(30, 46)]
+	for n in range(30, 46):
+		start = pc()
+		fib_py(n)
+		end = pc()
+		timep.append(end-start)
+		start2 = pc()
+		f = Integer(n)
+		f.fib()
+		end2 = pc()
+		timec.append(end2-start2)
+	plt.plot(nlist, timep)
+	plt.plot(nlist, timec)
+	plt.xlabel('Fibonacci number n')
+	plt.ylabel('Time (s)')
+	plt.savefig('timeplot.png')
+	
+	time = []
+	for n in range(1,31):
+		start3 = pc()
+		fib_py(n)
+		end3 = pc()
+		time.append(round(end3-start3, 8))
+	print(time)
+
+	f.set(47)
 	print(f.fib())
 
 if __name__ == '__main__':
